f34e5b21fceb37a996d8dbb363a35373418eed90
John Rey <adioskid@mail.ru>
2023-01-04 23:17:16 +0300

    password

--- src/SomePassword.java
+++ src/SomePassword.java
@@ -23,7 +23,6 @@ public class SomePassword {
         return result;
     }
 
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите логин: ");
