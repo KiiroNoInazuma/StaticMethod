064548cbf2f193177fca2d8b14d1219dcee0dd6d
John Rey <adioskid@mail.ru>
2023-01-04 23:15:21 +0300

    Initial commit

---
+++ src/SomePassword.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SomePassword {
+
+    public static boolean someMet(String login, String password, String confirmPassword) {
+        boolean result = false;
+        try {
+            if (login.matches("[a-zA-z0-9]*") && login.length() <= 20) {
+                if (password.matches("[a-zA-z0-9]*") && password.length() <= 20 && password.equals(confirmPassword)) {
+                    System.out.println("Пароль и логин введены верно!");
+                    result = true;
+                } else {
+                    throw new WrongPasswordException();
+                }
+            } else {
+                throw new WrongLoginException();
+            }
+        } catch (WrongLoginException e) {
+            System.out.println("Некорректный ввод логина!");
+        } catch (WrongPasswordException e) {
+            System.out.println("Некорректный ввод пароля!");
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите логин: ");
+        String login = sc.nextLine();
+        System.out.println("Введите пароль: ");
+        String password = sc.nextLine();
+        System.out.println("Подтвердите пароль: ");
+        String confirmPassword = sc.nextLine();
+        someMet(login, password, confirmPassword);
+    }
+}
