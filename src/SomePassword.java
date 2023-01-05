9f0f1e7cf80ca1b0a1b3c187e0baeb238e647743
John Rey <adioskid@mail.ru>
2023-01-05 14:50:44 +0300

    password

--- src/SomePassword.java
+++ src/SomePassword.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 
 public class SomePassword {
 
-    public static boolean someMet(String login, String password, String confirmPassword) {
-        boolean result = false;
+    public static void someMet(String login, String password, String confirmPassword) {
         try {
             if (login.matches("([a-zA-Z0-9]*).{3,21}") && login.length() <= 20) {
                 if (password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{4,20}$") && password.equals(confirmPassword)) {
                     System.out.println("Пароль и логин введены верно!");
-                    result = true;
                 } else {
                     throw new WrongPasswordException();
                 }
@@ -20,7 +18,6 @@ public class SomePassword {
         } catch (WrongPasswordException e) {
             System.out.println("Некорректный ввод пароля!");
         }
-        return result;
     }
 
     public static void main(String[] args) {
